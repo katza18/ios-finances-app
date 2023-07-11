a6287e6b0237dc2e1ee97c7262ccb8fb6907d05c
Aidan Katz <akatz42@gatech.edu>
2023-07-11 19:38:48 -0400

    Initial Commit

---
+++ Finances/FinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancesApp.swift
+//  Finances
+//
+//  Created by Aidan Katz on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
